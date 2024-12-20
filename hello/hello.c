1e9ee989ab88b46dd823f31c6642a9f908268034
Francesco1605 <fcastiglione551@gmail.com>
2024-12-20 15:44:34 +0000

    created hello.c

---
+++ hello/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//main function - the entry point
+// the return value of the main is an integer
+int main()
+{
+    //print the message "Hello World!!!" to the console
+    // \n is the shortcut for new line
+    printf("Hello World!!!\n\n\n");
+
+/* return 0:indicated taht the program is successfully */
+    return 0;
+}

e9475a1d9c22a72e0713769acc85837f5f1483a2
Francesco1605 <fcastiglione551@gmail.com>
2025-02-07 17:58:09 +0100

    esercizio4 verifica terminato

---
+++ Compito_intermedio/esercizio4_castiglione.h
@@ -0,0 +1,13 @@
+#ifndef ESERCIZIO4_CASTIGLIONE_H
+#define ESERCIZIO4_CASTIGLIONE_H
+
+// Funzione per stampare la matrice 3D:
+void print_matrix(int matrix[2][3][3]);
+
+// Funzione per modificare gli elementi pari e dispari:
+void modify_even_odd(int matrix[2][3][3]);
+
+// Funzione per verificare se una sottostruttura 3x3 è un Sudoku perfetto:
+int is_perfect_sudoku(int matrix[3][3]);
+
+#endif

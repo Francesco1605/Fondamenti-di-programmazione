4171545e8165cf94a2ee1f0466ff2f386e6c8516
Francesco1605 <fcastiglione551@gmail.com>
2025-02-07 14:44:42 +0100

    esercizio verifica terminato

---
+++ Compito_intermedio/esercizio3_castiglione.h
@@ -0,0 +1,8 @@
+#ifndef ESERCIZIO3_CASTIGLIONE_H
+
+#define ESERCIZIO3_CASTIGLIONE_H
+
+void mask_word(const char* word, char* masked_word);  // Dichiarazione per nascondere la parola
+int check_letter(const char* word, char* masked_word, char letter);  // Dichiarazione per verificare la lettera
+
+#endif

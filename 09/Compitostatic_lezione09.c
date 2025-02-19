c90b7b808b8035b1d41d957448b4fb7e4fbb1974
Francesco1605 <fcastiglione551@gmail.com>
2025-02-19 15:33:25 +0100

    update compito

--- 09/Compitostatic_lezione09.c
+++ 09/Compitostatic_lezione09.c
@@ -3,13 +3,9 @@
 #include "compitostatic_lezione09.h"
 
 int main() {
-    
     int total_words = 0;
-    int total_vowels = 0;
-    int total_consonants = 0;
-    int total_length = 0;
+    size_t total_length = 0; // Cambiato a size_t per essere coerente con strlen
     char word[100];
-    
 
     do {
         printf("Inserisci una parola: "); // Chiede all'utente di inserire una parola
@@ -19,20 +15,19 @@ int main() {
             break;
         } // Se l'utente inserisce "stop" il programma si ferma
 
-        size_t length = strlen(word);
         total_words++; // Somma totale delle parole analizzate
+        size_t length = strlen(word); // Calcola la lunghezza della stringa dopo aver letto l'input
         total_length += length; // Somma totale della lunghezza delle parole
 
-        analyze_word(word, &total_vowels, &total_consonants);
+        analyze_word(word, total_words, total_length);
 
-        printf("Totale vocali: %d, Totale consonanti: %d\n", total_vowels, total_consonants);
         printf("Totale parole analizzate: %d\n", total_words);
-        printf("Lunghezza parola: %d\n", length);
-        printf("Totale lunghezza parole: %d\n", total_length);
+        printf("Lunghezza parola: %zu\n", length); // Utilizza %zu per stampare size_t
+        printf("Totale lunghezza parole: %zu\n", total_length); // Utilizza %zu per stampare size_t
         printf("Media lunghezza parole: %.2f\n", (float)total_length / total_words);
         printf("\n");
 
-    } while (1); // Ciclo infinito
+    } while (1);
 
     return 0;
 }
--- 09/Funzioni.c
+++ 09/Funzioni.c
@@ -2,28 +2,30 @@
 #include <string.h>
 #include "compitostatic_lezione09.h"
 
-void analyze_word(char word[], int *vowels, int *consonants) {
+void analyze_word(char word[]) {
+    static int total_vowels = 0;
+    static int total_consonants = 0;
     int local_vowels = 0;
     int local_consonants = 0;
-    size_t length = strlen(word);
+    size_t length = strlen(word); // Calcola la lunghezza della stringa una volta
 
-        // Conteggio delle vocali e delle consonanti della parola inserita:
+    // Conteggio delle vocali e delle consonanti della singola parola:
     for (size_t i = 0; i < length; i++) {
         if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u' ||
             word[i] == 'A' || word[i] == 'E' || word[i] == 'I' || word[i] == 'O' || word[i] == 'U') {
-
             local_vowels++;
-
         } else if ((word[i] >= 'a' && word[i] <= 'z') || (word[i] >= 'A' && word[i] <= 'Z')) {
-
             local_consonants++;
         }
     }
 
     // Somma delle vocali e delle consonanti ad ogni parola:
-    *vowels += local_vowels;
-    *consonants += local_consonants;
+    total_vowels += local_vowels;
+    total_consonants += local_consonants;
 
-    printf("Parola: %s\n", word); // Parola inserita
-    printf("Vocali: %d, Consonanti: %d\n", local_vowels, local_consonants); // Vocali e consonanti della parola inserita
+    printf("Parola: %s\n", word);
+    printf("Vocali nella parola: %d\n", local_vowels);
+    printf("Consonanti nella parola: %d\n", local_consonants);
+    printf("Totale vocali: %d\n", total_vowels);
+    printf("Totale consonanti: %d\n", total_consonants);
 }

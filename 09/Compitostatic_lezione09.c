40ce70116cf4550b1b7af113e7225264480c6a1d
Francesco1605 <fcastiglione551@gmail.com>
2025-02-19 16:35:51 +0100

    update compito

--- 09/Compitostatic_lezione09.c
+++ 09/Compitostatic_lezione09.c
@@ -4,28 +4,20 @@
 
 int main() {
     static int total_words = 0;
-    static size_t total_length = 0; // Cambiato a size_t per essere coerente con strlen
+    static size_t total_length = 0;
     char word[100];
 
     do {
-        printf("Inserisci una parola: "); // Chiede all'utente di inserire una parola
+
+        printf("Inserisci una parola (stop per teminare): "); // Chiede all'utente di inserire una parola
         scanf("%s", word);
-
-        if (strcmp(word, "stop") == 0) {
-            break;
-        } // Se l'utente inserisce "stop" il programma si ferma
-
-        total_words++; // Somma totale delle parole analizzate
-        size_t length = strlen(word); // Calcola la lunghezza della stringa dopo aver letto l'input
-        total_length += length; // Somma totale della lunghezza delle parole
 
         analyze_word(word, total_words, total_length);
 
-        printf("Totale parole analizzate: %d\n", total_words);
-        printf("Lunghezza parola: %zu\n", length); // Utilizza %zu per stampare size_t
-        printf("Totale lunghezza parole: %zu\n", total_length); // Utilizza %zu per stampare size_t
-        printf("Media lunghezza parole: %.2f\n", (float)total_length / total_words);
-        printf("\n");
+        if (strcmp(word, "stop") == 0) {
+            printf("Programma terminato.\n");
+            break;
+        } // Se l'utente inserisce "stop" il programma si ferma
 
     } while (1);
 
--- 09/Funzioni.c
+++ 09/Funzioni.c
@@ -3,11 +3,15 @@
 #include "compitostatic_lezione09.h"
 
 void analyze_word(char word[]) {
+    
+    // Dichiarazione delle variabili:
     static int total_vowels = 0;
     static int total_consonants = 0;
     int local_vowels = 0;
     int local_consonants = 0;
-    size_t length = strlen(word); // Calcola la lunghezza della stringa una volta
+    static int total_words = 0;
+    static size_t total_length = 0; 
+    size_t length;
 
     // Conteggio delle vocali e delle consonanti della singola parola:
     for (size_t i = 0; i < length; i++) {
@@ -23,9 +27,20 @@ void analyze_word(char word[]) {
     total_vowels += local_vowels;
     total_consonants += local_consonants;
 
+    total_words++; // Somma totale delle parole analizzate
+    
+    length = strlen(word); // Calcola la lunghezza della stringa una volta sola
+    total_length += length; // Somma totale della lunghezza delle parole
+
     printf("Parola: %s\n", word);
     printf("Vocali nella parola: %d\n", local_vowels);
     printf("Consonanti nella parola: %d\n", local_consonants);
     printf("Totale vocali: %d\n", total_vowels);
     printf("Totale consonanti: %d\n", total_consonants);
+    printf("Totale parole analizzate: %d\n", total_words);
+    printf("Lunghezza parola: %zu\n", length); // Utilizza %zu per stampare size_t
+    printf("Totale lunghezza parole: %zu\n", total_length); // Utilizza %zu per stampare size_t
+    printf("Media lunghezza parole: %.2f\n", (float)total_length / total_words);
+    printf("\n");
+
 }

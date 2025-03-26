6b8ebe35c1c381113dfc6040be524478427a39ec
Francesco1605 <fcastiglione551@gmail.com>
2025-03-26 15:39:43 +0100

    compito terminato

--- 011-A/Compitoc++_lezione011.cpp
+++ 011-A/Compitoc++_lezione011.cpp
@@ -1,96 +1,101 @@
 #include <iostream>
-#include <vector>
 #include <fstream>
+#include <vector>
 #include <string>
 
 using namespace std;
 
-inline void end_ln(int x) {
-    for (int i = 0; i < x; i++) {
-        cout << endl;
-    }
-}
-
 class Person {
 protected:
-    int age;
-    string name;
-    string surname;
-
+    string name, surname;
 public:
-    Person() {}; 
-    Person(const string& n, const string& s, int a) : name(n), surname(s), age(a) {}; 
-    virtual ~Person() { cout << "~Person" << endl; } 
-
+    Person(const string& n, const string& s) : name(n), surname(s) {}
+    virtual ~Person() {}
     string getFullName() const { return name + " " + surname; }
-    int getAge() const { return age; }
-
-    void setName(const string& n) { name = n; }
-    void setSurname(const string& s) { surname = s; }
-    void setAge(int a) { age = a; }
 };
 
-class BasketPlayer : public Person {
+class Player : public Person {
 private:
     int number;
-    float pointsPerGame;
-    float reboundsPerGame;
-    float assistsPerGame;
-
+    float pointsPerGame, reboundsPerGame, assistsPerGame;
+    static int totalPlayers;
 public:
-    BasketPlayer(const string& n, const string& s, int a, int num, float ppg, float rpg, float apg) 
-        : Person(n, s, a), number(num), pointsPerGame(ppg), reboundsPerGame(rpg), assistsPerGame(apg) {}
+    Player(const string& n, const string& s, int num, float ppg, float rpg, float apg)
+        : Person(n, s), number(num), pointsPerGame(ppg), reboundsPerGame(rpg), assistsPerGame(apg) {
+        totalPlayers++;
+    }
 
-    void Print() const {
+    ~Player() {}
+
+    void print() const {
         cout << "---------------------------" << endl;
         cout << "Nome: " << getFullName() << endl;
-        cout << "Età: " << getAge() << endl;
-        cout << "Numero maglia: " << number << endl;
-        cout << "Punti a partita: " << pointsPerGame << endl;
-        cout << "Rimbalzi a partita: " << reboundsPerGame << endl;
-        cout << "Assist a partita: " << assistsPerGame << endl;
+        cout << "Numero di maglia: " << number << endl;
+        cout << "Punti per partita: " << pointsPerGame << endl;
+        cout << "Rimbalzi per partita: " << reboundsPerGame << endl;
+        cout << "Assist per partita: " << assistsPerGame << endl;
         cout << "---------------------------" << endl << endl;
     }
 
     float getPointsPerGame() const { return pointsPerGame; }
-    int getNumber() const { return number; }
+    static int getTotalPlayers() { return totalPlayers; }
 };
 
-int readPlayersFromFile(vector<BasketPlayer>& players, const string& filename) {
-    static int totalPlayersRead = 0;
+int Player::totalPlayers = 0;
 
+int readPlayersFromFile(vector<Player>& players, const string& filename) {
     ifstream file(filename);
     if (!file) {
-        cout << "Errore nell'apertura del file" << endl;
-        return -1;
+        cerr << "Errore nell'apertura del file: " << filename << endl;
+        return 0;
     }
 
     string firstName, lastName;
-    int age, number, count = 0;
+    int number;
     float ppg, rpg, apg;
+    int count = 0;
 
-    while (file >> firstName >> lastName >> age >> number >> ppg >> rpg >> apg) {
-        players.emplace_back(firstName, lastName, age, number, ppg, rpg, apg);
-        totalPlayersRead++;
+    while (file >> firstName >> lastName >> number >> ppg >> rpg >> apg) {
+        if (players.size() >= 10) break;
+        players.emplace_back(firstName, lastName, number, ppg, rpg, apg);
         count++;
     }
+    file.close();
 
-    cout << "[DEBUG]  Numero di giocatori letti: " << totalPlayersRead << endl << endl;
-    
+    cout << "Giocatori letti in questa chiamata: " << count << endl;
+    cout << "Totale giocatori letti finora: " << Player::getTotalPlayers() << endl << endl;
     return count;
 }
 
-int main() { 
-    cout << "Hello, World!" << endl;
-    end_ln(2);
-
-    vector<BasketPlayer> players;
-    readPlayersFromFile(players, "giocatori.txt");
-
-    cout << "--- Squadra letta dal file ---" << endl;
-    for (const auto& p : players) {
-        p.Print();
+Player bestScorer(const vector<Player>& players) {
+    if (players.empty()) {
+        cerr << "Errore: nessun giocatore disponibile." << endl;
+        exit(1);
     }
+
+    const Player* best = &players[0];
+    for (const auto& player : players) {
+        if (player.getPointsPerGame() > best->getPointsPerGame()) {
+            best = &player;
+        }
+    }
+    return *best;
+}
+
+int main() {
+    vector<Player> players;
+
+    readPlayersFromFile(players, "giocatori1.txt");
+    readPlayersFromFile(players, "giocatori2.txt");
+
+    cout << "--- Squadra ---" << endl;
+    for (const auto& player : players) {
+        player.print();
+    }
+
+    Player best = bestScorer(players);
+    cout << "Il miglior marcatore della squadra è: " << best.getFullName()
+         << " con " << best.getPointsPerGame() << " punti a partita." << endl;
 
     return 0;
 }
